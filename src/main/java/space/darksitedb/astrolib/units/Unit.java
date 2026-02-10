7869780f84ef5ef349f7f81277062a1d5bf8b98e
Anthony Paulin <apaulin@cisco.com>
2026-02-10 15:48:48 +1100

    Avoid long lines

--- src/main/java/space/darksitedb/astrolib/units/Unit.java
+++ src/main/java/space/darksitedb/astrolib/units/Unit.java
@@ -1,9 +1,11 @@
 package space.darksitedb.astrolib.units;
 
 /**
- * Abstract class representing a unit of measurement. It provides a common structure for all units, including a value and a method to retrieve it.
+ * Abstract class representing a unit of measurement. It provides a common structure for all units, 
+ * including a value and a method to retrieve it.
  * 
- * Value is stored as a double to allow for a wide range of measurements, including very small and very large values, which are common in astronomical contexts.
+ * Value is stored as a double to allow for a wide range of measurements, 
+ * including very small and very large values, which are common in astronomical contexts.
  */
 public abstract class Unit {
 
